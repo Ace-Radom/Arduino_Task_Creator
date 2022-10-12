48288cd9e6969d1ba45433db00ca71ed006d96a4
Sichen Lyu <104905842+Ace-Radom@users.noreply.github.com>
2022-10-12 13:39:49 +0200

    update cmdlib.h

--- include/cmdlib.h
+++ include/cmdlib.h
@@ -7,33 +7,36 @@
  * 
  * \brief give back commands expressions (show help by -?), connect @a cmdlib.json
  * \def \c namespace \c cmdlib
- *      \def \c __cwd_cmdlib__
  * 
  * start time: 2022.10.06
  * 
- * 2022.10.11 change \c Json::Value name from \c cmdlib \c rJson because of appeared errors in @a mingw32-make (same name)
+ * 2022.10.11 change \c Json::Value name from \c cmdlib \c cmdlib_json because of appeared errors in @a mingw32-make (same name)
  */
 
 #ifndef _CMDLIB_H_
 #define _CMDLIB_H_
 
-#include<jsoncpp>
+#include<iostream>
+#include<iomanip>
 #include<direct.h>
+#include<string>
+#include<fstream>
 
+#include<json/json.h>
+#include<collectinfo.h>
+
+/*
 #ifdef __cwd_cmdlib__
 
     #pragma message( "__cwd_cmdlib__ has already been defined as " __cwd_cmdlib__ )
 
 #endif
-
-Json::Value rJson;
-Json::Reader reader;
-std::fstream RWJson; // read and write .json (cmdlib.json)
+ */
 
 namespace cmdlib{
-    char* __cwd_cmdlib__;
 
     void show_help();
+    void setw_print_help( std::string , std::string );
 
 } // namespace cmdlib
 
