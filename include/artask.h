08033550ee903e11307ab1847ec3e2699d051556
Sichen Lyu <104905842+Ace-Radom@users.noreply.github.com>
2022-10-15 00:03:49 +0200

    add status lib

--- include/artask.h
+++ include/artask.h
@@ -6,6 +6,8 @@
  * ===============================================
  * 
  * \brief main library for Arduino_Task_Creator_Project
+ * \def \c STATUS = \c bool
+ * \def \c PATH   = \c std::string
  * \def \c class \c ARTast the main class for Arduino_Task
  * 
  * start time: 2022.10.11
@@ -16,16 +18,35 @@
 
 #include<string>
 
+typedef bool STATUS;
+typedef std::string PATH;
+
+#define _verify_ 0x00
+#define _upload_ 0x01
+
 class ARTast{
     public:
+        ARTast();
         
         void change_board();
         void change_serial_port();
 
     private:
+        void pre_set();
 
         std::string arduino_board;
         std::string serial_port;
+
+#pragma region status
+
+        STATUS is_help_triggered;
+        STATUS is_using_taskfile;
+        STATUS is_board_set;
+        STATUS is_serial_port_set;
+        STATUS is_file_set;
+        STATUS is_verify_or_upload;
+
+#pragma endregion status
 
 }; // class ARTast
 
