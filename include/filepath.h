9e56f353529d1a10553e7f5d378c38856e4de524
Ace_Radom <sichenradomlyu@gmail.com>
2022-10-18 15:32:51 +0200

    add relative path judge and fullpath func

--- include/filepath.h
+++ include/filepath.h
@@ -7,6 +7,9 @@
  * 
  * \brief functions for file-path process, such as judge if file / folder exists
  *                                                 see if there are writable or read-only
+ * \def \c namespace \c path
+ *      \def \c bool \c is_relative_path judge if this path is a relative path
+ *      \def \c void \c _fullpath change relative path to a full path
  * 
  * start time 2022.10.18
  */
@@ -15,5 +18,11 @@
 #define _FINEPATH_H_
 
 #include<collectinfo.h>
+#include<string>
+
+namespace path{
+    bool is_relative_path( std::string );
+    std::string _fullpath( std::string );
+}; // namespace path
 
 #endif
--- src/filepath.cpp
+++ src/filepath.cpp
@@ -1,2 +1,29 @@
 #include<filepath.h>
 
+/**
+ * \brief judge if this path is a relative path
+ * \param _path the path of a file or folder
+ * \return bool, true for this path is a relative path, false for it's not
+ */
+bool path::is_relative_path( std::string _path ){
+    if ( ( ( _path[0] >= 'a' && _path[0] <= 'z' ) || ( _path[0] >= 'A' && _path[0] <= 'Z' ) ) && _path[1] == ':' )
+    {
+        return false;
+        // this path is begin with a Latin script and the second letter is ':'
+        // that means that this path is a full path
+    }
+    return true;
+}
+
+
+/**
+ * \brief change relative path to a full path
+ * \param _path a relative path
+ * \return full path (add \c __cwd__ path at the front)
+ */
+std::string path::_fullpath( std::string _path )
+{
+    std::string _ans( clinfo::__cwd__ );
+    _ans.append( "\\" ).append( _path );
+    return _ans;
+}
