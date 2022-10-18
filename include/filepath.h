c80fb0b01cefc0f6d6af6ab6f732afcbbe20d660
Ace_Radom <sichenradomlyu@gmail.com>
2022-10-18 16:44:05 +0200

    add fp struct, get_file_permission func

--- include/filepath.h
+++ include/filepath.h
@@ -10,6 +10,11 @@
  * \def \c namespace \c path
  *      \def \c bool \c is_relative_path judge if this path is a relative path
  *      \def \c void \c _fullpath change relative path to a full path
+ *      \def \c struct \c fp file permission, save the permission this user has on this file
+ *           \def \c existence
+ *           \def \c execute_permission
+ *           \def \c write_permission
+ *           \def \c read_permission
  * 
  * start time 2022.10.18
  */
@@ -23,6 +28,15 @@
 namespace path{
     bool is_relative_path( std::string );
     std::string _fullpath( std::string );
+
+    struct fp{ // file permission
+        bool existence;
+        bool execute_permission;
+        bool write_permission;
+        bool read_permission;
+    };
+
+    void get_file_permission( std::string , fp & );
 }; // namespace path
 
 #endif
