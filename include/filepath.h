39fa76570286cb80f688caf5cc2eba85b192acee
Ace_Radom <sichenradomlyu@gmail.com>
2022-10-18 14:39:22 +0200

    add filepath lib

---
+++ include/filepath.h
@@ -0,0 +1,19 @@
+/**
+ * ===============================================
+ * ===============================================
+ * *\include\filepath.h
+ * ===============================================
+ * ===============================================
+ * 
+ * \brief functions for file-path process, such as judge if file / folder exists
+ *                                                 see if there are writable or read-only
+ * 
+ * start time 2022.10.18
+ */
+
+#ifndef _FILEPATH_H_
+#define _FINEPATH_H_
+
+#include<collectinfo.h>
+
+#endif
