f6f38e38f5ca37a056450912d2568ba3bc3b014f
Ace_Radom <sichenradomlyu@gmail.com>
2022-10-19 08:40:16 +0200

    add connection part declare to arduino_debug

--- include/artask.h
+++ include/artask.h
@@ -18,10 +18,14 @@
  *          \def \c ARTask create one ARTask class and pre-set
  *          \def \c void \c change_board change board type of arduino
  *          \def \c void \c change_serial_port change serial port to connect to arduino board
+ *          \def \c void \c read_taskfile read datas in taskfile
+ *          \def \c void \c start send command to @a arduino_debug
  *      \c private:
  *          \def \c void \c pre_set pre-set all vars in class
  *          \def \c arduino_board board type of arduino
  *          \def \c serial_port serial port to connect to arduino board
+ *          \def \c void \c make_command write command from taskfile to \c command
+ *          \def \c command the command needs to be sended to @a arduino_debug
  *          \def \c is_help_triggered judge if needs to show help. if typing in @a -h @a -? or not typing in any option, show help page and this will be \c _USING_
  *          \def \c is_using_taskfile judge if is using task file or not
  *          \def \c is_board_set judge if arduino board is set
@@ -65,11 +69,19 @@ class ARTask{
         void set_arduino_file( PATH_t );
         void set_verify_or_upload( STATUS_t );
 
+        void read_taskfile( PATH_t );
+
+        void start();
+
     private:
         void pre_set();
 
         std::string arduino_board;
         std::string serial_port;
+
+        void make_command();
+
+        std::string command;
 
 #pragma region status
 
