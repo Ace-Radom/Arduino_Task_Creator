cd97c9210f65e22e15f449e28b9d2673c849bb6a
Ace_Radom <sichenradomlyu@gmail.com>
2022-10-22 20:44:27 +0200

    finish func write_atccreatetasktemp_json, developing func start

--- src/artask.cpp
+++ src/artask.cpp
@@ -14,8 +14,9 @@
 
 /**
  * \brief create one ARTask class
+ * \param _no_use truly, exactly, 100% useless
  */
-ARTask::ARTask(){
+ARTask::ARTask( STATUS_t _no_use ){
     pre_set();
     return;
 }
@@ -85,21 +86,17 @@ void ARTask::set_verify_or_upload( STATUS_t _option ){
  * \brief only read when these data aren't set by type-in options
  * \param _path taskfile path
  */
-void ARTask::read_taskfile( PATH_t _path ){
+void ARTask::read_taskfile(){
     if ( !is_using_taskfile )
     {
         return;
         // here needs to be completed with the status not set a taskfile but function be triggered
     }
 
-    Json::Reader rJson; // Json reader
-    Json::Value artask_json;
-    std::ifstream rFile; // file reader
+    rwFile.open( taskfile_path.c_str() , std::ios::in );
+    rJson.parse( rwFile , artask_json , 0 );
 
-    rFile.open( _path.c_str() , std::ios::in );
-    rJson.parse( rFile , artask_json , 0 );
-
-    rFile.close();
+    rwFile.close();
 
 #pragma region read_taskfile
 
@@ -137,6 +134,64 @@ void ARTask::read_taskfile( PATH_t _path ){
 
 }
 
+/**
+ * \brief start sending command
+ */
+void ARTask::start(){
+    if ( is_using_taskfile )
+    {
+        read_taskfile();
+    }
+
+    if ( is_board_set            == _SET_ &&  // check if all settings making commands needs is ready, including Arduino board
+         is_serial_port_set      == _SET_ &&  //                                                                 serial port connects to Arduino board
+         is_arduino_file_set     == _SET_ &&  //                                                                 Arduino program file (.ino)
+         is_verify_or_upload_set == _SET_ )   //                                                                 program mode (verify or upload)
+    {
+        
+        make_command();
+        std::cout << command << std::endl;
+
+        //system( command.c_str() );
+        return;
+    }
+
+#pragma region error-occurs
+
+    if ( !is_board_set )
+    {
+        std::cout << "Arduino Board type not set"   << std::endl
+                  << "You can use \"-b\" to set it" << std::endl;
+    }
+
+    if ( !is_serial_port_set )
+    {
+        std::cout << "Serial port not set"                                                          << std::endl
+                  << "You can use \"-p\" to set it"                                                 << std::endl
+                  << "Maybe try \"-device\" to see which serial port does your Arduino connect to?" << std::endl;
+    }
+
+    if ( !is_arduino_file_set )
+    {
+        std::cout << "Arduino program file not set"                                   << std::endl
+                  << "Add Arduino program file path after \"-v\" or \"-u\" to set it" << std::endl;
+    }
+
+    if ( !is_verify_or_upload_set )
+    {
+        std::cout << "Program work mode not set (verify or upload)" << std::endl
+                  << "Add \"-v\" or \"-u\" option to set it"        << std::endl;
+    }
+
+#pragma endregion error-occurs
+
+    return;
+}
+
+void ARTask::test(){
+    write_atccreatetasktemp_json();
+}
+
 #pragma endregion public_function
 
 #pragma region private_function
@@ -158,4 +213,64 @@ void ARTask::pre_set(){
     is_verify_or_upload = _NOT_SET_;
 }
 
+/**
+ * \brief write Arduino_Task_Creator Task create temp json
+ */
+void ARTask::write_atccreatetasktemp_json(){
+    rwFile.open( taskfile_path.c_str() , std::ios::in );
+    rJson.parse( rwFile , artask_json , 0 );
+
+    rwFile.close();
+
+    Json::Value atc_createtask_temp;
+    Json::StyledStreamWriter sw; // json file writer
+
+    std::string program_work_mode;
+    
+    switch ( is_verify_or_upload ){
+        case _verify_:
+            program_work_mode = "verify_args";
+            break;
+        
+        case _upload_:
+            program_work_mode = "upload_args";
+            break;
+    }
+    // judge if is using verify or upload
+
+    atc_createtask_temp["label"] = artask_json["label"];
+    atc_createtask_temp["comand_mode"] = artask_json["commands"]["command_mode"];
+    atc_createtask_temp["args"].append( artask_json["commands"][program_work_mode] );
+    atc_createtask_temp["compiler"] = artask_json["compiler"];
+    atc_createtask_temp["compilerTag"] = artask_json["compilerTag"];
+    atc_createtask_temp["arduino_board"] =  Json::Value( arduino_board );
+    atc_createtask_temp["port"] = Json::Value( serial_port );
+    atc_createtask_temp["arduino_file"] = Json::Value( arduino_file_path );
+    // write all datas making command needs to atc_createtask_temp
+
+
+    rwFile.open( "atc_createtask_temp.json" , std::ios::out | std::ios::app );
+
+//  sw.write( std::cout , atc_createtast_temp );
+    // debug line
+
+    sw.write( rwFile , atc_createtask_temp );
+    // write atc_createtask_temp.json
+
+    artask_json.clear();
+    atc_createtask_temp.clear();
+    return;
+}
+
+/**
+ * 
+ */
+void ARTask::make_command(){
+
+}
+
 #pragma endregion private_function
+
+std::string delete_jsontag_sign( std::string origin_tag ){
+    return origin_tag.substr( 2 , origin_tag.length() - 3 );
+}
